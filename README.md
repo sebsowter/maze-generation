8383125f883190bc6a1a1a41419077b4f73ef89a
sebsowter <seb@sebeynott.com>
2022-02-20 19:12:48 +0000

    Updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Maze Generation
 
-Maze generation using [Phaser](https://phaser.io/) and the [Randomised Prim's algorithm](https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Prim's_algorithm). [View the demo](https://sebsowter.github.io/maze-generation/).
+Maze generation using [Phaser](https://phaser.io/) and the [Randomised Prim's algorithm](https://en.wikipedia.org/wiki/Maze_generation_algorithm#Randomized_Prim's_algorithm).
+
+[View the demo](https://sebsowter.github.io/maze-generation/).
 
 ![Maze](https://user-images.githubusercontent.com/7384630/81481446-36906000-9228-11ea-8b62-9fb5330ee174.png)
--- src/js/Scene.ts
+++ src/js/Scene.ts
@@ -1,5 +1,5 @@
-import 'phaser';
-	
+import "phaser";
+
 const enum Tiles {
   wall = 0,
   neighbour = 1,
@@ -17,26 +17,26 @@ export default class TableScene extends Phaser.Scene {
   private rows: integer = 496 / 16 - this.border * 2;
 
   public preload() {
-    this.load.image('tiles', './assets/tiles.png');
+    this.load.image("tiles", "./assets/tiles.png");
   }
-  
+
   public init() {
     for (let i = 0; i < this.cols * this.rows; i++) {
       this.tiles.push(Tiles.wall);
     }
   }
-  
+
   public create() {
     const tilemap = this.make.tilemap({
-      key: 'map',
+      key: "map",
       tileWidth: 16,
       tileHeight: 16,
       width: 31,
       height: 31,
     });
-    const tileset = tilemap.addTilesetImage('tileset', 'tiles', 16, 16);
-    
-    this.layer = tilemap.createBlankDynamicLayer('layer', tileset);
+    const tileset = tilemap.addTilesetImage("tileset", "tiles", 16, 16);
+
+    this.layer = tilemap.createBlankDynamicLayer("layer", tileset);
 
     for (let y = 0; y < this.rows + this.border * 2; y++) {
       for (let x = 0; x < this.cols + this.border * 2; x++) {
@@ -45,15 +45,17 @@ export default class TableScene extends Phaser.Scene {
     }
 
     const random = Math.floor(Math.random() * (this.tiles.length / 4));
-    const index = (Math.floor(random / (this.cols / 2)) * 2 * this.cols) + ((random % Math.ceil(this.cols / 2)) * 2);
+    const index =
+      Math.floor(random / (this.cols / 2)) * 2 * this.cols +
+      (random % Math.ceil(this.cols / 2)) * 2;
 
     this.addCellToMaze(index);
 
     this.timer = this.time.addEvent({
-      delay: 50,
+      delay: 10,
       loop: true,
       callbackScope: this,
-      callback: function() {
+      callback: function () {
         if (this.walls.length) {
           this.step();
         } else {
@@ -79,7 +81,7 @@ export default class TableScene extends Phaser.Scene {
       const opposite = neighbours[neighbour];
 
       this.setCell(index, Tiles.passage);
-    
+
       if (opposite !== null) {
         this.addCellToMaze(opposite);
       }
@@ -88,9 +90,180 @@ export default class TableScene extends Phaser.Scene {
     }
   }
 
+  private createRandomRectangle(
+    rectangle?: Phaser.Geom.Rectangle
+  ): Phaser.Geom.Rectangle {
+    const width = rectangle
+      ? Math.min(this.getRandomV(rectangle.width), 25)
+      : this.getRandomV(24);
+    const height = rectangle
+      ? Math.min(this.getRandomV(rectangle.height), 25)
+      : this.getRandomV(24);
+    const x = rectangle
+      ? Math.max(this.getRandomV(rectangle.x, true), 0)
+      : this.getRandomV(1, true);
+    const y = rectangle
+      ? Math.max(this.getRandomV(rectangle.y, true), 0)
+      : this.getRandomV(1, true);
+
+    return new Phaser.Geom.Rectangle(x, y, width, height);
+  }
+
+  private getRandomValue(reverse?: boolean): number {
+    const random = Math.floor(Math.random() * 5);
+    const value = random === 4 ? 1 : random >= 2 ? -1 : 0;
+
+    return reverse ? value - value : value;
+  }
+
+  private getRandomV(value: number, reverse?: boolean): number {
+    return Math.max(value + this.getRandomValue(reverse), 1);
+  }
+
+  private getMatrixFromRect(rectangles: Phaser.Geom.Rectangle[]): any[] {
+    const maxX = 8;
+    const maxY = 8;
+    const matrix = [];
+
+    for (let y = 0; y < maxY; y++) {
+      matrix[y] = [];
+
+      for (let x = 0; x < maxX; x++) {
+        matrix[y][x] = 0;
+      }
+    }
+
+    rectangles.forEach((rectangle) => {
+      for (let y = 0; y < maxY; y++) {
+        for (let x = 0; x < maxX; x++) {
+          const value =
+            x >= rectangle.x &&
+            x < rectangle.x + rectangle.width &&
+            y >= rectangle.y &&
+            y < rectangle.y + rectangle.height
+              ? 1
+              : matrix[y][x];
+
+          matrix[y][x] = value;
+        }
+      }
+    });
+
+    return matrix;
+  }
+
+  private createTower(): any[] {
+    const maxHeight = 12;
+    const matrix = [];
+
+    let rectangle1 = this.createRandomRectangle();
+    let rectangle2 = this.createRandomRectangle();
+
+    for (let i = 0; i < maxHeight; i++) {
+      rectangle1 = this.createRandomRectangle(i > 2 ? rectangle1 : null);
+      rectangle2 = this.createRandomRectangle(i > 2 ? rectangle2 : null);
+
+      matrix.push(this.getMatrixFromRect([rectangle1, rectangle2]));
+    }
+
+    return matrix;
+  }
+
+  private getNeighbours(tile: integer, direction: integer) {
+    const cell = this.indexToCell(tile, 8);
+    const right =
+      cell.x + 1 < 8 ? new Phaser.Math.Vector2(cell.x + 1, cell.y) : null;
+    const bottom =
+      cell.y + 1 < 8 ? new Phaser.Math.Vector2(cell.x, cell.y + 1) : null;
+    const left =
+      cell.x - 1 >= 0 ? new Phaser.Math.Vector2(cell.x - 1, cell.y) : null;
+    const top =
+      cell.y - 1 >= 0 ? new Phaser.Math.Vector2(cell.x, cell.y - 1) : null;
+    const cells = [right, top, left, bottom];
+    const neighbours = [];
+
+    for (let i = 0; i < cells.length; i++) {
+      neighbours.push(cells[Phaser.Math.Wrap(i + direction, 0, 4)]);
+    }
+
+    return neighbours;
+  }
+
+  private getStartTile(level: any[]): integer {
+    for (let y = level.length - 1; y >= 0; y--) {
+      const row = level[y];
+
+      for (let x = 0; x < row.length; x++) {
+        if (row[x] === 1) {
+          return this.cellToIndex(x, y, row.length);
+        }
+      }
+    }
+
+    return 0;
+  }
+
+  private cellToIndex(x: integer, y: integer, cols: integer): integer {
+    return y * cols + x;
+  }
+
+  private indexToCell(index: integer, cols: integer): Phaser.Math.Vector2 {
+    return new Phaser.Math.Vector2(index % cols, Math.floor(index / cols));
+  }
+
+  private addDoors(level: any[]) {
+    const length = 4 + Math.floor(Math.random() * 4);
+    const door = 1 + Math.floor(Math.random() * (length - 2));
+    const path = [];
+
+    let tile = this.getStartTile(level);
+    const cell = this.indexToCell(tile, 8);
+    let direction = 1;
+    console.log("-- tile", tile);
+    console.log("door", door);
+
+    path.push(tile);
+
+    level[cell.y][cell.x] = 4;
+
+    let count = 0;
+
+    while (path.length < length && count < 1000) {
+      const neighbours = this.getNeighbours(tile, direction);
+
+      let found = false;
+      let i = 0;
+
+      while (i < neighbours.length && !found) {
+        const neighbour = neighbours[i];
+
+        if (neighbour && level[neighbour.y][neighbour.x] === 1) {
+          found = true;
+          direction = -i + 1;
+          tile = this.cellToIndex(neighbour.x, neighbour.y, 8);
+          console.log("path.length", path.length);
+          path.push(tile);
+
+          level[neighbour.y][neighbour.x] =
+            path.length === door + 1 ? 1 : path.length === length ? 4 : 3;
+        }
+
+        i++;
+      }
+
+      //console.log('path.length', path.length);
+
+      count++;
+    }
+
+    console.log("count", count);
+
+    return level;
+  }
+
   private addCellToMaze(index: integer) {
     this.setCell(index, Tiles.maze);
-    this.getAdjacent(index).forEach(cell => {
+    this.getAdjacent(index).forEach((cell) => {
       if (cell !== null && this.tiles[cell] !== Tiles.passage) {
         this.setCell(cell, Tiles.neighbour);
       }
@@ -126,27 +299,30 @@ export default class TableScene extends Phaser.Scene {
   private getAdjacentX(index: integer, direction: integer): integer {
     const neighbour = index % this.cols;
 
-    if ((neighbour > 0 && direction < 0) || (neighbour < this.cols - 1 && direction > 0)) {    
+    if (
+      (neighbour > 0 && direction < 0) ||
+      (neighbour < this.cols - 1 && direction > 0)
+    ) {
       return index + direction;
     }
-    
+
     return null;
   }
 
   private getAdjacentY(index: integer, direction: integer): integer {
     const neighbour = index + direction * this.cols;
-    
+
     if (neighbour >= 0 && neighbour < this.tiles.length) {
       return neighbour;
     }
-    
+
     return null;
   }
 
   private getLayerPosition(index: integer): Phaser.Math.Vector2 {
     return new Phaser.Math.Vector2(
-      index % this.cols + this.border,
-      Math.floor(index / this.cols) + this.border,
+      (index % this.cols) + this.border,
+      Math.floor(index / this.cols) + this.border
     );
   }
 }
